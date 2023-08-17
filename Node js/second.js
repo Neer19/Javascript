e2ef242874a1081a6dcdb31d08def558429d1fe0
Neer Ghevariya <92365281+Neer19@users.noreply.github.com>
2023-08-17 23:35:51 +0530

    Add files via upload

---
+++ Node js/second.js
@@ -0,0 +1,11 @@
+// (function(exports, require, module, __filename, __dirname){
+
+neer = {
+  name: "Neer",
+  favNum: 20,
+  developer: true,
+};
+// console.log(exports, require, module, __filename, __dirname)
+module.exports = neer;
+
+// })
